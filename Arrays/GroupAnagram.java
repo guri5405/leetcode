d9d333095acf9179205c8a3789cb2444c716739d
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2024-01-18 11:36:11 +0530

    Create GroupAnagram.java

---
+++ Arrays/GroupAnagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+      // extract string characters  into word 
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);// sorting the characters of string
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+  
+  public static void main(String [] args){
+    String strs = {"eat","tea","tan","ate","nat","bat"};
+    System.out.println(groupAnagram(strs));
+}
